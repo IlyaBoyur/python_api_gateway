c085bda78dbaf5e2516acf39eb12a2d1ae84a3d6
Ilya Boyur <ilya.boyur.r@ya.ru>
2025-05-12 21:31:27 +0300

    Add genres to out schema

--- src/api/v1/schemas/film.py
+++ src/api/v1/schemas/film.py
@@ -16,10 +16,10 @@ class FilmOutSchema(BaseOutSchema):
     title: str = Field(description="Название")
     description: str = Field(description="Описание")
     imdb_rating: float = Field(description="Рейтинг")
+    genres: str = Field(description="Жанры")
     actors: list[ActorOutSchema] = Field(description="Актеры", default_factory=list)
     directors: list[DirectorOutSchema] = Field(description="Режиссеры", default_factory=list)
     writers: list[WriterOutSchema] = Field(description="Сценаристы", default_factory=list)
-    # created: datetime = Field(description="Дата создания")
 
     @classmethod
     def from_entity(cls, film: Film) -> "FilmOutSchema":
@@ -28,10 +28,10 @@ class FilmOutSchema(BaseOutSchema):
             title=film.title,
             description=film.description,
             imdb_rating=film.imdb_rating,
+            genres=film.genres,
             actors=[ActorOutSchema.from_entity(actors) for actors in film.actors],
             directors=[DirectorOutSchema.from_entity(directors) for directors in film.directors],
             writers=[WriterOutSchema.from_entity(writer) for writer in film.writers],
-            # created=film.created,
         )
 
 
